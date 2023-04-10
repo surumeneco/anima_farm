d712df95e8be83749d95499b4d42e6534e4dee68
surumeneco <surumeneko164@gmail.com>
2023-04-10 16:14:16 +0900

    一部オブジェクト実装

---
+++ loading/panel_loading.js
@@ -0,0 +1,3 @@
+document.write('<script src="./panel_object/farm_panel.js"></script>');
+document.write('<script src="./panel_object/out_panel.js"></script>');
+
---
+++ loading/scene_loading.js
@@ -0,0 +1,7 @@
+document.write('<script src="./scene/general/title_scene.js"></script>');
+document.write('<script src="./scene/general/home_scene.js"></script>');
+document.write('<script src="./scene/farm/rear_scene.js"></script>');
+document.write('<script src="./scene/farm/shop_scene.js"></script>');
+document.write('<script src="./scene/farm/marge_scene.js"></script>');
+document.write('<script src="./scene/farm/breed_scene.js"></script>');
+
