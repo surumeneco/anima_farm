08402078c07a4ac4ea17718dd115c0c68541b955
surumeneco <surumeneko164@gmail.com>
2023-04-12 09:31:49 +0900

    オブジェクト関係ドキュメント追加

--- loading/object_loading.js
+++ loading/object_loading.js
@@ -1,3 +1,4 @@
 document.write('<script src="./global_object/game_button.js"></script>');
 document.write('<script src="./global_object/menubutton.js"></script>');
+document.write('<script src="./global_object/anima.js"></script>');
 
